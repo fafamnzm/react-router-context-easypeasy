851612e06ca42d818f3a3d38ed8590049b5f377f
faramarz monazami haghighat <51343277+fafamnzm@users.noreply.github.com>
2020-03-15 17:45:48 +0330

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            <h1>About Us</h1>
+            <h1>You already know about the website on the Home page
+                for more detail about me go to https://www.faramarz-monazami.com
+            </h1>
         </div>
     )
 }
