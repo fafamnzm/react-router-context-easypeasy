4565b81c2c01444c6933c1b9517427b467905cb6
faramarz monazami haghighat <51343277+fafamnzm@users.noreply.github.com>
2020-03-15 17:44:26 +0330

    updating the info

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,8 @@ export default function Home() {
     return (
         <div>
             <h1>
-                Welcome to My test Shopping site
+                Welcome to My test React site in which I use Context API and 
+                easy-peasy which is an easier version of redux
             </h1>
         </div>
     )
